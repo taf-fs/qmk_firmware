652ef7a4b9642d06b4e46963cbd26cc4058abae9
Petr Sedlacek <petr@sedlacek.biz>
2023-01-19 14:32:33 +0100

    42. Keebs Discipad: fix keycodes, config.h deprecations

--- keyboards/42keebs/discipad/keymaps/via/keymap.c
+++ keyboards/42keebs/discipad/keymaps/via/keymap.c
@@ -17,7 +17,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_numpad_5x4_full(
-      KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+      KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
       KC_P7,   KC_P8,   KC_P9,   KC_A,
       KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
       KC_P1,   KC_P2,   KC_P3,   KC_B,
@@ -44,6 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [_BL] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
 };
 #endif
